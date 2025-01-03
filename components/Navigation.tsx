fdb3d3d3b0610ab729fc9733f63b041eb466a34c
Mesfin Girma <mesfingirmatwo@gmail.com>
2025-01-03 15:27:18 +0300

    implemented navigation bar

---
+++ components/Navigation.tsx
@@ -0,0 +1,36 @@
+import { UserButton } from '@clerk/nextjs'
+import { User } from '@clerk/nextjs/server'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+type Props = {
+    user?: null | User
+}
+
+const Navigation = ({user}: Props) => {
+  return (
+    <div className='p-4 flex items-center justify-between relative'>
+      <aside className='flex items-center gap-2 '>
+        <Image src={'./assets/plura-logo.svg'} alt='plur logo' width={40} height={40} />
+        <span className='text-xl font-bold'>Plura.</span>
+      </aside>
+      <nav className='hidden md:block absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%]'>
+       <ul className='flex items-center gap-8 justify-center'>
+        <Link  href={'#'}>Pricing</Link>
+        <Link  href={'#'}>Pricing</Link>
+        <Link  href={'#'}>Pricing</Link>
+        <Link  href={'#'}>Pricing</Link>
+
+       </ul>
+      </nav>
+      <aside>
+        <Link href={'/agency'} className='bg-primary text-white p-2 px-4 rounded-md hover:bg-primary/80'>Login</Link>
+        <UserButton />
+        <ModeToggle />
+      </aside>
+    </div>
+  )
+}
+
+export default Navigation
